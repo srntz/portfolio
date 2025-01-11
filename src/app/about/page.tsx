ec0982625a4152dd51400ce8ec6aae3e8dc99c6c
-srntz <tuzsocial909@gmail.com>
2025-01-11 15:02:33 -0700

    Add scroll-triggered style change for NavbarAlternate. Add main heading and image to the about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import NavbarAlternate, { ILink } from "@/components/NavbarAlternate";
+import { useState } from "react";
+import { motion } from "motion/react";
+import Image from "next/image";
 
 const aboutPageNavbarLinks: ILink[] = [
   {
@@ -18,15 +21,45 @@ const aboutPageNavbarLinks: ILink[] = [
 ];
 
 export default function About() {
+  const [mainContentRendered, setMainContentRendered] = useState(false);
   return (
     <div id="about">
-      <header className="flex justify-center pt-5">
-        <NavbarAlternate links={aboutPageNavbarLinks} />
+      <header className="sticky z-[100] mt-5 top-5 flex justify-center">
+        <NavbarAlternate
+          links={aboutPageNavbarLinks}
+          callback={() => setMainContentRendered(true)}
+          styleChangeScrollPosition={115}
+        />
       </header>
       <div id="main-content" className="px-40">
-        <h1 className="text-[5rem] font-nohemi my-12 font-bold">
-          Hi, I'm Denis.
-        </h1>
+        {mainContentRendered && (
+          <>
+            <motion.h1
+              initial={{ top: -15, opacity: 0 }}
+              animate={{ top: 0, opacity: 1 }}
+              className="relative text-[5.9rem] font-nohemi my-12 font-bold"
+            >
+              Hi, I'm Denis.
+            </motion.h1>
+            <section className="w-full">
+              <motion.div
+                className="relative"
+                initial={{ top: -20, opacity: 0 }}
+                animate={{ top: 0, opacity: 1 }}
+                transition={{ delay: 0.2 }}
+              >
+                <Image
+                  width={500}
+                  height={1200}
+                  src={"/assets/myPhoto.jpg"}
+                  alt={"personal photo"}
+                  className="rounded-3xl"
+                ></Image>
+              </motion.div>
+              <div className="h-[100vh]"></div>
+            </section>
+          </>
+        )}
       </div>
     </div>
   );
--- src/components/NavbarAlternate.tsx
+++ src/components/NavbarAlternate.tsx
@@ -1,6 +1,10 @@
-import { motion } from "motion/react";
-import Image from "next/image";
-import { useState } from "react";
+import {
+  motion,
+  useAnimate,
+  useMotionValueEvent,
+  useScroll,
+} from "motion/react";
+import { useRef, useState } from "react";
 import { useNavigationContext } from "@/context/NavigationContext";
 import BreatingLogo from "@/components/BreathingLogo";
 
@@ -9,22 +13,74 @@ export interface ILink {
   href: string;
 }
 
-export default function NavbarAlternate({ links }: { links: ILink[] }) {
+export default function NavbarAlternate({
+  links,
+  callback,
+  styleChangeScrollPosition,
+}: {
+  links: ILink[];
+  callback?: () => void;
+  styleChangeScrollPosition: number;
+}) {
   const [containerAnimationCompleted, setContainerAnimationCompleted] =
     useState(false);
 
+  const navbarFilterVisible = useRef(false);
+
   const { navigate } = useNavigationContext();
+
+  const [scope, animate] = useAnimate();
+
+  const { scrollY } = useScroll();
+
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    if (latest > styleChangeScrollPosition && !navbarFilterVisible.current) {
+      animate(
+        scope.current,
+        {
+          backgroundColor: "rgba(29,29,29,0)",
+        },
+        { duration: 0.25, ease: "linear" },
+      );
+      navbarFilterVisible.current = true;
+    } else if (
+      latest < styleChangeScrollPosition &&
+      navbarFilterVisible.current
+    ) {
+      animate(
+        scope.current,
+        { backgroundColor: "#1d1d1d" },
+        { duration: 0.25, ease: "linear" },
+      );
+      navbarFilterVisible.current = false;
+    }
+  });
 
   return (
     <motion.nav
-      initial={{ top: -30, opacity: 0, width: 130 }}
-      animate={{ opacity: 1, width: "45rem", top: "auto" }}
-      onAnimationComplete={() => setContainerAnimationCompleted(true)}
+      ref={scope}
+      initial={{
+        top: -30,
+        opacity: 0,
+        width: 130,
+      }}
+      animate={{
+        opacity: 1,
+        width: "45rem",
+        top: 0,
+      }}
+      onAnimationComplete={() => {
+        setContainerAnimationCompleted(true);
+        if (callback) {
+          callback();
+        }
+      }}
       transition={{
         default: { duration: 0.15, delay: 0.3, ease: "easeInOut" },
         width: { delay: 0.5, duration: 0.2, ease: "easeInOut" },
       }}
-      className="relative flex items-center px-6 bg-[#1D1D1D] h-[4.5rem] backdrop-blur-lg rounded-3xl"
+      style={{ backdropFilter: "brightness(0.5) blur(10px)" }}
+      className="relative bg-[#1D1D1D] flex items-center px-6 h-[4.5rem] rounded-3xl"
     >
       <BreatingLogo delay={0.5} size={35} />
       {containerAnimationCompleted && (
